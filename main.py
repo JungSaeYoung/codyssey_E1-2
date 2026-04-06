d983be9630acaaef41b1273ab20ee16998d26ef0
JungSaeYoung <ashofrondol@gmail.com>
2026-04-07 04:43:58 +0900

    feat: macOS ctrl+z 예외 처리

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from game import QuizGame
+import signal
 
 def main():
+    def handle_suspend(signum, frame):
+        # Ctrl+Z로 프로그램이 일시 중지되는 것을 방지하기 위해 시그널 핸들러를 설정합니다.(macOS에서는 SIGTSTP가 Ctrl+Z에 해당합니다.)
+        print("\nCtrl+Z는 사용할 수 없습니다.")
+
+    signal.signal(signal.SIGTSTP, handle_suspend)
+
     game = QuizGame()
     
     try:
